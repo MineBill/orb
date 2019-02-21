2d9b0014ceeda5b550ae294a91668f2c587be6d4
MineBill <terrestrialstudio@gmail.com>
2019-02-21 18:31:34 +0200

    Removed spdlog.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
-#include <fmt/format.h>
+#include "include/spdlog/spdlog.h"
 
 #include <include/slang.h>
 #include "include/Lexer.hpp"
 #include "include/Token.hpp"
 
 int main(int argc, char *argv[]) {
+    spdlog::warn("HEY");
     Lexer lexer("______ = 2;");
     TokenList list = lexer.lex();
     for(auto token: list){
-        fmt::print("Token = ({},{})\n", token.type, token.value);
+        // fmt::print("Token = ({},{})\n", token.type, token.value);
     }
 
     return 0;
