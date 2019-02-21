47506df4181000f1e36f632fdaa3015ed1ffee76
MineBill <terrestrialstudio@gmail.com>
2019-02-21 16:52:56 +0200

    Init

---
+++ include/slang.h
@@ -0,0 +1,9 @@
+#ifndef SLANG_H
+#define SLANG_H
+
+/* This generated file contains includes for project dependencies */
+#include "bake_config.h"
+
+
+#endif
+
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include <include/slang.h>
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
